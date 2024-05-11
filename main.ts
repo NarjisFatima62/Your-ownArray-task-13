60f1dad1c484b474512e999b3714f5f6def03db7
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 21:13:30 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//Question Number:13
+//Your OwnArray: Think of your favourite mode of transportation, sach as a motorcycle or a car,& make a list that stores several eaxamples.Use your list to print a series of statements about these items, sach as "I would like to own a Honda motorcycle".
+
+//let transportation: string[] = ["Honda motorcycle","Audi","Honda City"];
+//transportation.map((items)=> console.log(`I would like to own a ${items}`));
+
+//Question Number: 14
+//Guest list: If you can invite anyone, living or deceased, to dinner,who would you invite? Make a list that include at least three person you'd like to invite to dinner. Then use your list to print a message to each person,inviting them to dinner. 
+
+let guestArr: string [] = ["Sara","Dua","Asia","Zia"];
+guestArr.map((items)=> (console.log(`Dear ${items}, you are invited to dinner`)));
